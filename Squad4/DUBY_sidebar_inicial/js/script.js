b47b2b8b6c745309b9d8464955837f9f5fe90a33
igor-merten <55738849+igor-merten@users.noreply.github.com>
2025-05-06 12:30:16 -0300

    Adicionado grafico de repasses por período utilizando chart.js, removido função de fechar/abrir sidebar ao dar scroll

---
+++ Squad4/DUBY_sidebar_inicial/js/script.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+       
+    const ctx = document.getElementById('graficoRepassesPeiodo').getContext('2d');
+
+    const data = {
+        labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+        datasets: [
+            {
+            label: 'Repasse',
+            data: [1000, 2000, 1500, 2500, 1300, 500, 1400, 2200, 2500, 1000, 1500, 2000],
+            borderColor: '#412884',
+            fill: 
+            {
+                target: 'origin',
+                above: 'rgba(65, 40, 132, 0.4)',   // Cor da area acima da origem
+            },
+            stepped: "middle",
+            }
+        ]
+    };
+
+    new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options: {
+            responsive: true,
+            interaction: {
+                intersect: false,
+                axis: 'x'
+            },
+            
+        plugins: {
+            title: {
+                display: true,
+                text: 'Repasses por Período',
+                color: "#412884",
+                align: "start",
+                font: {size : 20},
+                padding: {bottom: 23}
+            },
+            legend: {
+                display: false
+            },
+            tooltip: {
+                enabled: true,
+                position: 'nearest',
+                displayColors: false   // remove quadrado ao lado do tooltip
+            }
+        },
+        scales: {
+        y: {
+            beginAtZero: true
+        }, 
+        }}
+    });
+
+});
