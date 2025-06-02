faa10305c866044822e0ffa73df718a10ab15752
igor-merten <55738849+igor-merten@users.noreply.github.com>
2025-06-02 19:28:38 -0300

    Alteração de diretório ao fazer logout

--- assets/js/dashboard/abridged.js
+++ assets/js/dashboard/abridged.js
@@ -1,6 +1,6 @@
 button = document.getElementById("btnLogout");
 
 button.addEventListener("click", function() {
-      window.open("../../../login/index.html", "_self")
+      window.open("../../../Zeta/login/index.html", "_self")
     }
 );
