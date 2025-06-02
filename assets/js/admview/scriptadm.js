82d91721ec2fc4f005e6cc207247965147f42e00
CBMSU <caio.bmartins@souunit.com.br>
2025-06-02 19:30:36 -0300

    Update scriptadm.js

--- assets/js/admview/scriptadm.js
+++ assets/js/admview/scriptadm.js
@@ -51,7 +51,7 @@ function resetarFiltros() {
 function logout() {
     const confirmLogout = confirm("Tem certeza que deseja sair?");
     if (confirmLogout) {
-        window.location.href = "Squad4/Tela de Login/index.html"; 
+        window.location.href = "../../../Zeta/login/index.html"; 
     }
 }
 
